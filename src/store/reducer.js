cc7f9737c6704edcee35fba941c5912bce81babc
Graham Bewley <grahambewley@gmail.com>
2019-11-27 14:29:21 -0500

    Switch to switch-case reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,112 +10,112 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_NEW_ADDITION':
+            console.log('[reducer.js] action = ', action);
+
+            // Immutably copy the current state.additions
+            let additionsCopy = Object.assign({}, state.additions);
+
+            if(additionsCopy[state.newAddition.time] !== undefined) {
+                additionsCopy[state.newAddition.time].push(state.newAddition);
+            } else {
+                additionsCopy[state.newAddition.time] = [];
+                additionsCopy[state.newAddition.time].push(state.newAddition);
+            }
+            console.log('[reducer.js] new additions = ', additionsCopy);
+
+            return {
+                ...state,
+                additions: {
+                    ...state.additions,
+                    ...additionsCopy
+                },
+                newAddition: {
+                    name: null,
+                    type: null,
+                    amount: null,
+                    time: null
+                }
+            }
+
+        case 'DELETE_ADDITION':
+            console.log('[reducer.js] action = ', action);
+            let delAdditionsCopy = Object.assign({}, state.additions);
+
+            delete delAdditionsCopy[action.additionTime];
+            console.log('[reducer.js] new additions = ',delAdditionsCopy);
+
+            return {
+                ...state,
+                additions: {
+                    ...delAdditionsCopy
+                },
+                newAddition: {
+                    ...state.newAddition
+                }
+            }
+        case 'RESTART_BREW':
+            return initialState;
+        
+        case 'RESTORE_FROM_STORAGE':
+            return {
+                ...state,
+                additions: {
+                    ...action.additions
+                },
+                boilMinutes: +action.boilMinutes,
+                newAddition: {
+                    ...state.newAddition
+                }
+            } 
+        case 'CLEAR_NEW_ADDITION':
+            return {
+                ...state,
+                additions: {
+                    ...state.additions
+                },
+                newAddition: {
+                    name: null,
+                    type: null,
+                    amount: null,
+                    time: null
+                }
+            }
+
+        case 'NEW_ADDITION_UPDATE':
+            return {
+                ...state,
+                additions: {
+                    ...state.additions
+                },
+                newAddition: {
+                    name: action.new.name,
+                    type: action.new.type,
+                    amount: action.new.amount,
+                    time: parseInt(action.new.time)
+                }
+            }
+
+        case 'BOIL_MINS_ADJUST':
+            console.log('[reducer] Boil Minutes Adjustment of ', action.adjustment);
+            let mins = state.boilMinutes;
+            mins += action.adjustment;
     
-    if(action.type === 'ADD_NEW_ADDITION') {
-        console.log('[reducer.js] action = ', action);
-
-        // Immutably copy the current state.additions
-        let additionsCopy = Object.assign({}, state.additions);
-
-        if(additionsCopy[state.newAddition.time] !== undefined) {
-            additionsCopy[state.newAddition.time].push(state.newAddition);
-        } else {
-            additionsCopy[state.newAddition.time] = [];
-            additionsCopy[state.newAddition.time].push(state.newAddition);
-        }
-        console.log('[reducer.js] new additions = ', additionsCopy);
-
-        return {
-            ...state,
-            additions: {
-                ...state.additions,
-                ...additionsCopy
-            },
-            newAddition: {
-                name: null,
-                type: null,
-                amount: null,
-                time: null
+            return {
+                ...state,
+                additions: {
+                    ...state.additions
+                },
+                boilMinutes: mins,
+                newAddition: {
+                    ...state.newAddition
+                }
             }
-        }
+        
+        default:
+            return state;
     }
-    if(action.type === 'DELETE_ADDITION') {
-        console.log('[reducer.js] action = ', action);
-        let additionsCopy = Object.assign({}, state.additions);
-
-        delete additionsCopy[action.additionTime];
-        console.log('[reducer.js] new additions = ',additionsCopy);
-
-        return {
-            ...state,
-            additions: {
-                ...additionsCopy
-            },
-            newAddition: {
-                ...state.newAddition
-            }
-        }
-    }
-    if(action.type === 'RESTART_BREW'){
-        return initialState;
-    }
-    if(action.type === 'RESTORE_FROM_STORAGE') {
-        return {
-            ...state,
-            additions: {
-                ...action.additions
-            },
-            boilMinutes: +action.boilMinutes,
-            newAddition: {
-                ...state.newAddition
-            }
-        } 
-    }
-    if(action.type === 'CLEAR_NEW_ADDITION') {
-        return {
-            ...state,
-            additions: {
-                ...state.additions
-            },
-            newAddition: {
-                name: null,
-                type: null,
-                amount: null,
-                time: null
-            }
-        }
-    }
-    if(action.type === 'NEW_ADDITION_UPDATE') {
-        return {
-            ...state,
-            additions: {
-                ...state.additions
-            },
-            newAddition: {
-                name: action.new.name,
-                type: action.new.type,
-                amount: action.new.amount,
-                time: parseInt(action.new.time)
-            }
-        }
-    }
-    if(action.type === 'BOIL_MINS_ADJUST') {
-        console.log('[reducer] Boil Minutes Adjustment of ', action.adjustment);
-        let mins = state.boilMinutes;
-        mins += action.adjustment;
-
-        return {
-            ...state,
-            additions: {
-                ...state.additions
-            },
-            boilMinutes: mins,
-            newAddition: {
-                ...state.newAddition
-            }
-        }
-    }
-    return state;
 };
 
 export default reducer;
